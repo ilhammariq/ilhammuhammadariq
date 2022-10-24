48449ef367b73b4e9be3591e8da02a7a1f4e0597
ilhammariq <ilhammuhammadariq123@gmail.com>
2022-10-24 18:38:38 +0700

    [Update] fix bug profil image for safari browser

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -28,7 +28,7 @@ const HomeComponent = () => {
           </div>
         </div>
         <div className='w-full flex justify-center md:pb-10'>
-          <svg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'>
+          <svg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg' className='w-full h-full'>
             <mask id='mask0' mask-type='alpha'>
               <path
                 d='M64.5,-36.6C74.2,-20.3,66.4,6.8,52.7,27.9C39.1,49,19.5,64.1,-0.6,64.4C-20.7,64.7,-41.4,50.3,-55.3,29C-69.3,7.8,-76.6,-20.3,-66.2,-36.9C-55.9,-53.6,-27.9,-58.7,-0.3,-58.6C27.3,-58.4,54.7,-52.9,64.5,-36.6Z'
--- src/components/SkillsComponent.js
+++ src/components/SkillsComponent.js
@@ -16,8 +16,8 @@ const SkillsComponent = () => {
           <div className="hidden md:grid grid-cols-3 gap-10 place-items-center">
             {
               iconSkills.map((item, id) => (
-                <div className='flex flex-col items-center gap-5'>
-                  <Icon key={id} className={`saturate-0 ${item.name === 'Bootstrap' && 'hover:contrast-100 contrast-0'} hover:saturate-100 cursor-pointer duration-500 hover:scale-110`} icon={item.icon} width={item.width} height={item.height} />
+                <div className='flex flex-col items-center gap-5'  key={id}>
+                  <Icon className={`saturate-0 ${item.name === 'Bootstrap' && 'hover:contrast-100 contrast-0'} hover:saturate-100 cursor-pointer duration-500 hover:scale-110`} icon={item.icon} width={item.width} height={item.height} />
                   <span className='md:text-lg text-sm font-medium dark:text-slate-400 text-slate-500'>{item.name}</span>
                 </div>
               ))
@@ -26,8 +26,8 @@ const SkillsComponent = () => {
           <div className="md:hidden grid grid-cols-3 place-items-center">
             {
               iconSkills.map((item, id) => (
-                <div className='py-5 flex flex-col items-center gap-5'>
-                  <Icon key={id} className={`saturate-0 ${item.name === 'Bootstrap' && 'hover:contrast-100 contrast-0'} hover:saturate-100 cursor-pointer duration-500 hover:scale-110`} icon={item.icon} width='60' height='60' />
+                <div className='py-5 flex flex-col items-center gap-5' key={id}>
+                  <Icon className={`saturate-0 ${item.name === 'Bootstrap' && 'hover:contrast-100 contrast-0'} hover:saturate-100 cursor-pointer duration-500 hover:scale-110`} icon={item.icon} width='60' height='60' />
                   <span className='md:text-lg text-sm font-medium dark:text-slate-400 text-slate-500'>{item.name}</span>
                 </div>
               ))
