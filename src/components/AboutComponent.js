bc9a3a8012395ccccd9d6798528a5f3a92a62399
ilhammariq <ilhammuhammadariq123@gmail.com>
2022-10-24 07:03:22 +0700

    [Update] add link about me

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -22,22 +22,22 @@ const AboutComponent = () => {
 
         <div className='grid grid-cols-4 place-items-center md:px-24 md:m-auto md:w-3/4 mb-10 px-5 text-dark dark:text-light'>
           <a
-            href='/'
+            href='https://www.instagram.com/ilhammuhammadariq/' rel="noopener noreferrer" target="_blank" 
             className='border-2 p-3 rounded-full cursor-pointer hover:text-light dark:hover:text-dark hover:bg-dark dark:hover:bg-light duration-700'>
             <Icon icon='line-md:instagram' width='25' height='25' />
           </a>
           <a
-            href='/'
+            href='https://www.linkedin.com/in/ilhammuhammadariq/' rel="noopener noreferrer" target="_blank"
             className='border-2 p-3 rounded-full cursor-pointer hover:text-light dark:hover:text-dark hover:bg-dark dark:hover:bg-light duration-700'>
             <Icon icon='line-md:linkedin' width='25' height='25' />
           </a>
           <a
-            href='/'
+            href='https://www.facebook.com/ilham.muhammadariq/' rel="noopener noreferrer" target="_blank"
             className='border-2 p-3 rounded-full cursor-pointer hover:text-light dark:hover:text-dark hover:bg-dark dark:hover:bg-light duration-700'>
             <Icon icon='line-md:facebook' width='25' height='25' />
           </a>
           <a
-            href='/'
+            href='https://github.com/ilhammariq' rel="noopener noreferrer" target="_blank"
             className='border-2 p-3 rounded-full cursor-pointer hover:text-light dark:hover:text-dark hover:bg-dark dark:hover:bg-light duration-700'>
             <Icon icon='line-md:github' width='25' height='25' />
           </a>
